8b05c9fecad9feaace8c7797797136f47e1ec0ae
Michael Schmitz <michaelschmitz5@gmail.com>
2016-08-04 19:47:00 -0600

    fix: navbar-collapse

--- js/agency.js
+++ js/agency.js
@@ -19,9 +19,12 @@
     });
 
     // Closes the Responsive Menu on Menu Item Click
-    $('.navbar-collapse ul li a:not(.dropdown-toggle)').click(function() {
-        $('.navbar-toggle:visible').click();
+    $('.navbar-collapse ul li a').click(function(){ 
+            $('.navbar-toggle:visible').click();
     });
+    // $('.navbar-collapse ul li a:not(.dropdown-toggle)').click(function() {
+    //     $('.navbar-toggle:visible').click();
+    // });
 
     // Offset for Main Navigation
     $('#mainNav').affix({
